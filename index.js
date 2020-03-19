ff61daf7e4045502996d8b289f1e7e49c4e5611d
Prem Kumar Ponuthorai <ppremk@github.com>
2020-03-19 21:29:20 +0100

    add pseudo logic

--- index.js
+++ index.js
@@ -1,20 +1,88 @@
-const core = require('@actions/core');
-
+const core = require("@actions/core")
+const github = require("@actions/github")
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const fsl = core.getInput('filesizelimit');
+  try {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    // const myToken = core.getInput("GITHUB_TOKEN")
+
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN)
+    const context = github.context
+
+    // Get owner, repo, and event from context of payload that triggered the action
+    const { owner, repo } = context.repo
+
+    const event_type = context.eventName
+    let issue_pr_number
+
+    const fsl = core.getInput("filesizelimit")
     console.log(`Default configured filesizelimit is set to ${fsl} bytes...`)
 
-    core.setOutput('action start time', new Date().toTimeString());
+    core.setOutput("action start time", new Date().toTimeString())
 
-    // do logic in here
+    // Start Logic here
 
-    core.setOutput('action end time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+    // Get PR number
+    if (event_type === "pull_request") {
+      issue_pr_number = context.payload.pull_request.number
+      core.setOutput("Is pull request detected?", "Yes")
+    } else {
+      core.setOutput("Is pull request detected?", "No")
+    }
+
+    //Get List of Files in PR
+    let pr_files
+    pr_files = await octokit.pulls.listFiles({
+      owner,
+      repo,
+      issue_pr_number
+    })
+    console.log(pr_files)
+
+    let pr_files_details
+    pr_files.array.forEach(element => {
+      pr_files_details = {
+        file_name: element.filename,
+        file_git_sha: element.sha
+      }
+    })
+    console.log(pr_files_details)
+
+
+    // Check Blob of file
+    let pr_files_blob_size
+    pr_files_details.array.forEach( element => {
+      pr_files_blob_size = await octokit.git.getBlob({
+        owner,
+        repo,
+        file_sha : element.file_git_sha
+      });
+    })
+    console.log(pr_files_blob_size)
+
+    
+
+
+    // Compare size of Blob with filesizelimit threshold
+
+    // TODO
+
+    // Create a Issue Comment if size is overlimit
+    await octokit.github.issues.createComment({
+      owner,
+      repo,
+      issue_number: issue_pr_number,
+      body:
+        "Max file sizelimit detected, consider using Git-LFS before merging this file in your target branch"
+    })
+
+    core.setOutput("action end time", new Date().toTimeString())
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
