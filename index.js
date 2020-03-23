fb1c8a4e9c307fed9085a9379d219865949a3d86
Prem Kumar Ponuthorai <ppremk@github.com>
2020-03-23 18:16:41 +0100

    refactor code and add label detection logic 4

--- index.js
+++ index.js
@@ -19,30 +19,35 @@ async function run() {
     console.log(`Name of Repository is ${repo} and the owner is ${owner}`)
     console.log(`Triggered event is ${event_type}`)
 
-    // Get LFS Warning Label
-    let lfslabel = await octokit.issues.getLabel({
-      owner,
-      repo,
-      name: ":warning: lfs-detected!"
-    })
+    let lfslabel
+    try {
+      // Get LFS Warning Label
+      lfslabel = await octokit.issues.getLabel({
+        owner,
+        repo,
+        name: ":warning: lfs-detected!"
+      })
 
-    console.log(lfslabel)
+      console.log(lfslabel)
 
-    // console.log(`Repo has lfs warning label - ${lfslabel}`)
+      // console.log(`Repo has lfs warning label - ${lfslabel}`)
 
-    // if (lfslabel === undefined) {
-    //   await octokit.issues.createLabel({
-    //     owner,
-    //     repo,
-    //     name: ":warning: lfs-detected!",
-    //     color: "ffcf00",
-    //     description:
-    //       "Warning Label for use when LFS is detected in the commits of a Pull Request"
-    //   })
+      // if (lfslabel === undefined) {
+      // }
+      
+    } catch (error) {
+        await octokit.issues.createLabel({
+          owner,
+          repo,
+          name: ":warning: lfs-detected!",
+          color: "ffcf00",
+          description:
+            "Warning Label for use when LFS is detected in the commits of a Pull Request"
+        })
 
-    //   console.log(`No lfs warning label detected. Creating new label ...`)
-    //   console.log(`LFS warning label created`)
-    // }
+        console.log(`No lfs warning label detected. Creating new label ...`)
+        console.log(`LFS warning label created`)      
+    }
 
     // Get List of files for Pull Request
     if (event_type === "pull_request") {
