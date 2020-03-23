4ec4479e78eeb30293de2163ff1856d3124c4ed3
Prem Kumar Ponuthorai <ppremk@github.com>
2020-03-23 17:09:55 +0100

    refactor code and add label detection logic

--- index.js
+++ index.js
@@ -12,11 +12,35 @@ let issue_pr_number
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const fsl = core.getInput("filesizelimit")
-
     console.log(`Default configured filesizelimit is set to ${fsl} bytes...`)
     console.log(`Name of Repository is ${repo} and the owner is ${owner}`)
     console.log(`Triggered event is ${event_type}`)
+
+    // Get LFS Warning Label
+    let lfslabel = await octokit.issues.getLabel({
+      owner,
+      repo,
+      name: ":warning: lfs-detected!"
+    })
+
+    console.log(`Repo has lfs warning label - ${lfslabel}`)
+
+    if (lfslabel === undefined) {
+      await octokit.issues.createLabel({
+        owner,
+        repo,
+        name: ":warning: lfs-detected!",
+        color: "ffcf00",
+        description:
+          "Warning Label for use when LFS is detected in the commits of a Pull Request"
+      })
+
+      console.log(`No lfs warning label detected. Creating new label ...`)
+      console.log(`LFS warning label created`)
+    }
+
+    // Get List of files for Pull Request
+    const fsl = core.getInput("filesizelimit")
 
     if (event_type === "pull_request") {
       issue_pr_number = context.payload.pull_request.number
@@ -28,8 +52,8 @@ async function run() {
         repo,
         pull_number: issue_pr_number
       })
-      console.log("Before Getting Size Property")
-      console.log(pullRequest) 
+      console.log("Before Getting Size Property") // for debug - remove for production
+      console.log(pullRequest)  // for debug - remove for production
 
       let newPRobj
       let prFilesWithBlobSize = await Promise.all(
@@ -50,24 +74,24 @@ async function run() {
         })
       )
 
-      console.log("After Getting Size Property")
-      console.log(prFilesWithBlobSize)
+      console.log("After Getting Size Property")  // for debug - remove for production
+      console.log(prFilesWithBlobSize)  // for debug - remove for production
 
       let lfsFile = []
-      for(let prop in prFilesWithBlobSize){
-        if (prFilesWithBlobSize[prop].fileblobsize > fsl){
+      for (let prop in prFilesWithBlobSize) {
+        if (prFilesWithBlobSize[prop].fileblobsize > fsl) {
           lfsFile.push(prFilesWithBlobSize[prop].filename)
         }
       }
 
-      console.log("Detected large file(s):")
-      console.log(lfsFile)
-
-      if (lfsFile.length > 0) {   
+      if (lfsFile.length > 0) {
         
-        lfsFile.join('\n')
+        console.log("Detected large file(s):")
+        console.log(lfsFile)
+
+        lfsFile.join("\n")
         let bodyTemplate = `## :warning: Possible large file(s) detected :warning: \n
-        The following file(s) exceeds the file size limit: ${fsl} bytes, as set in the .yml configuration files \n
+        The following file(s) exceeds the file size limit: ${fsl} bytes, as set in the .yml configuration files
         
         ${lfsFile.toString()}`
 
@@ -76,17 +100,20 @@ async function run() {
           repo,
           issue_number: issue_pr_number,
           body: bodyTemplate
-
         })
+      } else {
+
+        console.log("No large file(s) detected...")
+
       }
 
       // TODO:
-      // Format body reply to have multiline for each array ellement
+
       // logic to add lfs-file warning label in Pr
       // logic to set PR status as failed
 
-      // git lfs attributes misconfiguration
-
+      // git lfs attributes misconfiguration lfs watch dog logic
+      
     } else {
       console.log(`No Pull Request detected. Skipping LFS warning check`)
     }
