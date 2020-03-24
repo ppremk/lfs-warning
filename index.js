889fe373874097319c4a98f728f3e62e822cbfaf
Prem Kumar Ponuthorai <ppremk@github.com>
2020-03-24 23:27:23 +0100

    refactor code and add label detection logic 14

--- index.js
+++ index.js
@@ -8,44 +8,37 @@ const { owner, repo } = context.repo
 const event_type = context.eventName
 
 let issue_pr_number
-const labels = [":warning: lfs-detected!"]
+const labels = ["lfs-detected!"]
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-
     const fsl = core.getInput("filesizelimit")
 
     console.log(`Default configured filesizelimit is set to ${fsl} bytes...`)
     console.log(`Name of Repository is ${repo} and the owner is ${owner}`)
     console.log(`Triggered event is ${event_type}`)
 
-    try {
-      // Get LFS Warning Label
-      let lfslabelObj = await octokit.issues.getLabel({
+    // Get LFS Warning Label
+    let lfslabelObj = await octokit.issues.getLabel({
+      owner,
+      repo,
+      name: "lfs-detected!"
+    })
+
+    if (Object.entries(lfslabelObj).length === 0 && lfslabelObj.constuctor === Object) {
+      await octokit.issues.createLabel({
         owner,
         repo,
-        name: labels
+        name: "lfs-detected!",
+        color: "F92672",
+        description:
+          "Warning Label for use when LFS is detected in the commits of a Pull Request"
       })
-
-      for (let prop in lfslabelObj) {
-        lfslabel.push(lfslabelObj[prop].name)
-      }
-
-      console.log(`Repo has label - ${lfslabel}`)
-
-    } catch (error) {
-        await octokit.issues.createLabel({
-          owner,
-          repo,
-          name: ":warning: lfs-detected!",
-          color: "F92672",
-          description:
-            "Warning Label for use when LFS is detected in the commits of a Pull Request"
-        })
-
-        console.log(`No lfs warning label detected. Creating new label ...`)
-        console.log(`LFS warning label created`)      
+      console.log(`No lfs warning label detected. Creating new label ...`)
+      console.log(`LFS warning label created`)
+    } else {
+      console.log(`Repo has label - ${lfslabelObj}`)
     }
 
     // Get List of files for Pull Request
@@ -79,7 +72,7 @@ async function run() {
         })
       )
 
-      console.log(prFilesWithBlobSize)  // for debug - remove for production
+      console.log(prFilesWithBlobSize) // for debug - remove for production
 
       let lfsFile = []
       for (let prop in prFilesWithBlobSize) {
@@ -89,7 +82,6 @@ async function run() {
       }
 
       if (lfsFile.length > 0) {
-        
         console.log("Detected large file(s):")
         console.log(lfsFile)
 
@@ -105,8 +97,8 @@ async function run() {
           repo,
           issue_number: issue_pr_number,
           labels
-        });
-        
+        })
+
         await octokit.issues.createComment({
           owner,
           repo,
@@ -115,16 +107,13 @@ async function run() {
         })
 
         core.setOutput("lfsFiles", lfsFile)
-        core.setFailed(`Large File detected! Setting PR status to failed. Consider using git-lfs to track the LFS files`)        
-
+        core.setFailed(`Large File detected! Setting PR status to failed. Consider using git-lfs to track the LFS files`)
       } else {
-
         console.log("No large file(s) detected...")
-
       }
 
       // TODO:
-      // git lfs attributes misconfiguration lfs watch dog logic
+      // git lfs attributes misconfiguration aka missing installation on client while git-lfs is configured on repo upstream
 
     } else {
       console.log(`No Pull Request detected. Skipping LFS warning check`)
