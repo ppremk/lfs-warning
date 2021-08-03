6d2d81d88f017ad0999ecec9419371db99ab0697
Naseem <naseemkullah@gmail.com>
2021-08-02 20:42:28 -0400

    prevent accidentally checking in LFS stored files
    use git check-attr to determine if any PR files should be stored in LFS.
    
    Then among the PR files that should be stored in LFS (if any), check for
    the string version https://git-lfs.github.com/spec/v1 to determine if the
    file is indeed a pointer or not.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-const octokit = github.getOctokit(core.getInput("token"));
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import {exec} from 'child_process';
+import {promisify} from 'util';
+const execP = promisify(exec);
+const octokit = github.getOctokit(core.getInput('token'));
 const context = github.context;
-const {owner, repo} = context.repo;
+const {repo} = context;
 const event_type = context.eventName;
-
-const labels = ['lfs-detected!'];
 
 // most @actions toolkit packages have async methods
 async function run() {
@@ -13,21 +14,21 @@ async function run() {
     const fsl = core.getInput('filesizelimit');
 
     core.info(`Default configured filesizelimit is set to ${fsl} bytes...`);
-    core.info(`Name of Repository is ${repo} and the owner is ${owner}`);
+    core.info(`Name of Repository is ${repo.repo} and the owner is ${repo.owner}`);
     core.info(`Triggered event is ${event_type}`);
+
+    const labelName = 'lfs-detected!'
 
     // Get LFS Warning Label
     try {
       await octokit.rest.issues.getLabel({
-        owner,
-        repo,
-        name: 'lfs-detected!',
+        ...repo,
+        name: labelName,
       });
     } catch (error) {
       if (error.message === 'Not Found') {
         await octokit.rest.issues.createLabel({
-          owner,
-          repo,
+          ...repo,
           name: 'lfs-detected!',
           color: 'ff1493',
           description:
@@ -51,17 +52,15 @@ async function run() {
       core.info(`The PR number is: ${pullRequestNumber}`);
 
       const {data: files} = await octokit.rest.pulls.listFiles({
-        owner,
-        repo,
+        ...repo,
         pull_number: pullRequestNumber,
       });
 
       const prFilesWithBlobSize = await Promise.all(
         files.map(async file => {
-          const {filename, sha} = file;
+          const {filename, sha, patch} = file;
           const {data: blob} = await octokit.rest.git.getBlob({
-            owner,
-            repo,
+            ...repo,
             file_sha: sha,
           });
 
@@ -69,44 +68,72 @@ async function run() {
             filename,
             filesha: sha,
             fileblobsize: blob.size,
+            patch,
           };
         })
       );
 
       core.info(String(prFilesWithBlobSize));
 
-      const lsfFiles = [];
+      const largeFiles: string[] = [];
+      const accidentallyCheckedInLsfFiles: string[] = [];
       for (const file of prFilesWithBlobSize) {
         const {fileblobsize, filename} = file;
         if (fileblobsize !== null && fileblobsize > Number(fsl)) {
-          lsfFiles.push(filename);
+          largeFiles.push(filename);
+        } else {
+          // look for files below threshold that should be stored in LFS but are not
+          const shouldBeStoredInLFS = (
+            await execP(`git check-attr filter ${filename}`)
+          ).stdout.includes('filter: lfs');
+
+          if (shouldBeStoredInLFS) {
+            const isStoredInLFS = Boolean(
+              file.patch?.includes('version https://git-lfs.github.com/spec/v1')
+            );
+            if (!isStoredInLFS) {
+              accidentallyCheckedInLsfFiles.push(filename);
+            }
+          }
         }
       }
 
+      const lsfFiles = largeFiles.concat(accidentallyCheckedInLsfFiles);
+
       if (lsfFiles.length > 0) {
-        core.info('Detected large file(s):');
+        core.info('Detected file(s) that should be in LFS:');
         core.info(String(lsfFiles));
 
-        const lfsFileNames = lsfFiles.join(', ');
-        const bodyTemplate = `## :warning: Possible large file(s) detected :warning: \n
-            The following file(s) exceeds the file size limit: ${fsl} bytes, as set in the .yml configuration files
+        const largeFilesBody = `The following file(s) exceeds the file size limit: ${100} bytes, as set in the .yml configuration files:
 
-            ${lfsFileNames.toString()}
+        ${largeFiles.join(', ')}
 
-            Consider using git-lfs as best practises to track and commit file(s)`;
+        Consider using git-lfs to manage large files.
+      `;
+
+        const accidentallyCheckedInLsfFilesBody = `The following file(s) are tracked in LFS and were likely accidentally checked in:
+
+        ${accidentallyCheckedInLsfFiles.join(', ')}
+      `;
+
+        const body = `## :warning: Possible file(s) that should be tracked in LFS detected :warning:
+        ${largeFiles.length > 0 ? largeFilesBody : ''}
+        ${
+          accidentallyCheckedInLsfFiles.length > 0
+            ? accidentallyCheckedInLsfFilesBody
+            : ''
+        }`;
 
         await octokit.rest.issues.addLabels({
-          owner,
-          repo,
+          ...repo,
           issue_number: pullRequestNumber,
-          labels,
+          labels: [labelName],
         });
 
         await octokit.rest.issues.createComment({
-          owner,
-          repo,
+          ...repo,
           issue_number: pullRequestNumber,
-          body: bodyTemplate,
+          body,
         });
 
         core.setOutput('lfsFiles', lsfFiles);
@@ -115,10 +142,20 @@ async function run() {
         );
       } else {
         core.info('No large file(s) detected...');
-      }
 
-      // TODO:
-      // git lfs attributes misconfiguration aka missing installation on client while git-lfs is configured on repo upstream
+        const {data: labels} = await octokit.rest.issues.listLabelsOnIssue({
+          ...repo,
+          issue_number: 1,
+        })
+        if (labels.map(l => l.name).includes(labelName)) {
+          await octokit.rest.issues.removeLabel({
+            ...repo,
+            issue_number: 1,
+            name: labelName
+          });
+          core.info(`label ${labelName} removed`)
+        }
+      }
     } else {
       core.info('No Pull Request detected. Skipping LFS warning check');
     }
